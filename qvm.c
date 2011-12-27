883080a591801a8d49a15677c73009bea40de5b0
Yves Vandriessche <yvdriess@vub.ac.be>
2011-12-28 00:36:38 +0100

    - fixed adding tangles and qubits, without any real quantum state though - added some printing for debugs

--- qvm.c
+++ qvm.c
@@ -8,8 +8,7 @@
 #include <sexp_vis.h>
 
 #define STRING_SIZE sizeof(char)
-#define MIN_TANGLE_SIZE 8
-#define MAX_TANGLES sizeof(qid_t)
+#define MAX_TANGLES ((size_t)32)
 
 #define car hd_sexp
 #define cdr next_sexp
@@ -29,12 +28,11 @@ typedef struct tangle {
   //TODO: amp vec here
  } tangle_t;
 
-void init_tangle( tangle_t* tangle) {
-  assert( tangle == NULL );
-
-  tangle = (tangle_t*) malloc(sizeof(tangle_t));   //ALLOC tangle
+tangle_t* init_tangle() {
+  tangle_t* tangle = (tangle_t*) malloc(sizeof(tangle_t));   //ALLOC tangle
   tangle->size = 0;
   tangle->qids = NULL;
+  return tangle;
 }
 
 bool find_qid( qid_t qid, qid_list_t* restrict qids ) {
@@ -48,14 +46,12 @@ bool find_qid( qid_t qid, qid_list_t* restrict qids ) {
       return false;
 }
 
-void add_qid( qid_t qid, qid_list_t* qids ) {
-  if( qids ) // follow list until end
-    add_qid( qid, qids->rest );
-  else {
-    qids = (qid_list_t*) malloc(sizeof(qid_list_t*));
-    qids->qid = qid;
-    qids->rest = NULL;
-  }
+qid_list_t* add_qid( qid_t qid, qid_list_t* qids ) {
+  // assuming qid is NOT already in qids
+  qid_list_t* new_qids = (qid_list_t*) malloc(sizeof(qid_list_t*));
+  new_qids->qid = qid;
+  new_qids->rest = qids;
+  return new_qids;
 }
 
 void append_qids( qid_list_t* new_qids, qid_list_t* target_qids ) {
@@ -68,7 +64,7 @@ void append_qids( qid_list_t* new_qids, qid_list_t* target_qids ) {
 }
 
 void remove_qid( qid_t qid, qid_list_t* qids ) {
-
+  
 }
 
 void free_qid_list( qid_list_t* qids) {
@@ -91,54 +87,103 @@ typedef struct qmem {
   tangle_t** tangles;
 } qmem_t;
 
-void init_qmem(qmem_t* qmem) {
-  qmem = malloc(sizeof(qmem_t)); //ALLOC qmem
+void print_tangle( const tangle_t* restrict tangle ) {
+  assert( tangle );
+  printf("[");
+  for( qid_list_t* qids = tangle->qids;
+       qids;
+       qids=qids->rest ) {
+    printf("%d", qids->qid);
+    if( qids->rest )
+      printf(", ");
+  }
+  printf("]");
+}
+
+void print_qmem( const qmem_t* restrict qmem ) {
+  assert(qmem);
+  printf("qmem has %d tangles:\n  {", (int)qmem->size);
+  for( int i=0, tally=0 ; tally < qmem->size ; ++i ) {
+    assert(i<MAX_TANGLES);
+    if( qmem->tangles[i] ) {
+      if( tally>0 )
+	printf(",\n   ");
+      print_tangle(qmem->tangles[i]);
+      ++tally;
+    }
+  }
+  printf("}\n");
+  
+}
+
+qmem_t* init_qmem() {
+  qmem_t* qmem = malloc(sizeof(qmem_t)); //ALLOC qmem
   qmem->size = 0;
   qmem->tangles = calloc(MAX_TANGLES,sizeof(tangle_t*)); //ALLOC tangles
+  return qmem;
 }
 
 void free_qmem(qmem_t* qmem) {
-  for(int i=0; i < qmem->size; ++i)
-    free_tangle((tangle_t*)qmem->tangles[i]);  
+  for( int i=0, tally=0 ; tally < qmem->size ; i++ ) {
+    assert(i<MAX_TANGLES);
+    if( qmem->tangles[i] ) {
+      free_tangle((tangle_t*)qmem->tangles[i]);
+      ++tally;
+    }
+  }
   free(qmem->tangles); //FREE tangles
   free(qmem); //FREE qmem
-  qmem = NULL;
 }
 
 tangle_t* get_tangle(const qid_t qid, const qmem_t* qmem) {
   tangle_t* tangle = NULL;
-  for(int i=0; i<qmem->size; ++i) {
+  for( int i=0, tally=0 ; tally < qmem->size ; ++i ) {
     tangle = qmem->tangles[i];
-    if( find_qid(qid, tangle->qids) )
-      return tangle;      
+    if( tangle ) {
+      if( find_qid(qid, tangle->qids) )
+	return tangle;      
+      ++tally;
+    }
   }
   return NULL;
 }
 
 tangle_t* get_free_tangle(qmem_t* qmem) {
   // I loop here because tangles can get de-allocated (NULL-ed)
-  for(int i=0; i<qmem->size; ++i) {
-    if( qmem->tangles[i] == NULL )
+  printf("    searching for free tangle\n");
+  for(int i=0; i<MAX_TANGLES; ++i) {
+    if( qmem->tangles[i] == NULL ) {
+      printf("      found one at %d!\n",i);
+      qmem->tangles[i] = init_tangle();
       return qmem->tangles[i];
+    }
+    printf("      ...skipping %d\n",i);
   }
-  printf("Ran out of qmem memory, too many tangles!");
+  printf("Ran out of qmem memory, too many tangles! (> %lu)\n",MAX_TANGLES);
   exit(EXIT_FAILURE);
 }
 
 tangle_t* add_tangle(const qid_t qid1, 
 		     const qid_t qid2, 
-		     qmem_t* qmem) {
-  tangle_t* tangle = get_free_tangle(qmem);
-  init_tangle(tangle);
+		     qmem_t* restrict qmem) {
+  // allocate new tangle in qmem
+  tangle_t*  restrict tangle = get_free_tangle(qmem);
+
+  // init tangle
+  tangle->qids = add_qid( qid1, tangle->qids );
+  tangle->qids = add_qid( qid2, tangle->qids );
   tangle->size = 2;
-  add_qid( qid1, tangle->qids );
-  add_qid( qid2, tangle->qids );
-  // init quantum state
+
+  // update qmem info
+  qmem->size += 1;
+
+  // TODO init quantum state
+
   return tangle;
 }
 
 tangle_t* add_qubit(const qid_t qid, tangle_t* tangle) {
-  add_qid( qid, tangle->qids );
+  tangle->qids = add_qid( qid, tangle->qids );
   tangle->size += 1;
   // tensor |+> to tangle
   return tangle;
@@ -192,7 +237,9 @@ void eval_E(sexp_t* exp, qmem_t* qmem) {
   tangle_t* tangle_1;
   tangle_t* tangle_2;
   const tangle_t* new_tangle;
- 
+
+  assert( qmem );
+
   // move to the first argument
   exp = cdr(exp);
   if( !exp ) {
@@ -215,27 +262,31 @@ void eval_E(sexp_t* exp, qmem_t* qmem) {
   // get tangle for qid1
   tangle_1 = get_tangle( qid1, qmem );
 
-  printf("    grabbing tangle for qubit %d\n", qid1);
+  printf("    grabbing tangle for qubit %d\n", qid2);
   // get tangle for qid2
   tangle_2 = get_tangle( qid2, qmem );
 
-  if (tangle_1 && tangle_2) {
+  if (tangle_1==NULL && tangle_2==NULL) {
     // if both NULL, create new tangle with two |+> states
+    printf("    both qubits are fresh, creating new tangle\n");
     new_tangle = add_tangle(qid1, qid2, qmem);
   }
   else {
-    if ((tangle_1==NULL) || (tangle_2==NULL)) {
+    if ( tangle_1 || tangle_2 ) {
       // if either NULL
-      if (tangle_1) {
+      if( tangle_1 ) {
 	// add qid2 to tangle_1
+	printf("    adding fresh qubit %d to an existing tangle\n",qid2);
 	new_tangle = add_qubit( qid2, tangle_1 );
       }
       else {
+	assert( tangle_2 );
 	// add qid1 to tangle_2
+	printf("    adding fresh qubit %d to an existing tangle\n",qid1);
 	new_tangle = add_qubit( qid1, tangle_2 );
       }
     }
-    else{
+    else {
       // both tangles are non-NULL, merge both
       new_tangle = merge_tangles(tangle_1, tangle_2, qmem);
     }
@@ -244,10 +295,12 @@ void eval_E(sexp_t* exp, qmem_t* qmem) {
 }
 
 // expects a list, evals the first argument and calls itself tail-recursively
-void eval( sexp_t* exp, qmem_t* qmem ) {
+void eval( sexp_t* restrict exp, qmem_t* restrict qmem ) {
   sexp_t* command;
   sexp_t* rest;
   char opname;
+
+  assert( qmem );
 
   if( exp == NULL )
     return;
@@ -291,9 +344,12 @@ int main(int argc, char* argv[]) {
   // emit dot file
   /* sexp_to_dotfile( mc_program->list, "mc_program.dot" ); */
 
-  init_qmem( qmem );
+  qmem = init_qmem();
   
   eval( mc_program->list, qmem );
+
+  printf("Resulting quantum memory is:\n");
+  print_qmem( qmem );
 
   free_qmem( qmem );
   printf("ok\n");
