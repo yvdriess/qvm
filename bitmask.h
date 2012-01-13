44707195c36bdf5607b9e06673fe019fe4f6fc22
Yves Vandriessche <yvdriess@vub.ac.be>
2012-01-13 14:09:54 +0100

    should be feature complete, w3.mc still fails for some reason added interactive mode and checking the env for angle constants

---
+++ bitmask.h
@@ -0,0 +1,10 @@
+#include <limits.h>
+
+// from the comp.lang.c FAQ
+
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
