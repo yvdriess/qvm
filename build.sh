fd1440fd9d7500de66def4d52439f17134ff237d
yvdriess <yvdriess@vub.ac.be>
2014-09-29 13:50:17 +0200

    build and test changes

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-gcc -O0 -Lsexp/lib/ -Isexp/include/ -o qvm qvm.c -lsexp -lm -std=c99 -g3 -fopenmp
+gcc -O3 -Lsexp/lib/ -Isexp/include/ -o qvm qvm.c -lsexp -lm -std=c99 -g3 -fopenmp
+gcc -O3 -Lsexp/lib/ -Isexp/include/ -o qvm_ qvm.c -lsexp -lm -std=c99 -g3 -fopenmp -DNOPERM
